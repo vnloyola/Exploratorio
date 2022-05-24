e5808942c92e721cde669f5be1cd884debacda4b
VicenteL <vnloyola@uc.cl>
2022-05-23 22:32:43 -0400

    first commit

---
+++ key.py
@@ -0,0 +1,1 @@
+print("".join([chr(ord(_) - ord(__) + 55) for _, __ in zip("Qk_pR^YX=So	RZ	_WV8^2Shra9[h`bY7<dq<3jjR]=ijXkC,c;b]p8o*U`,1kdX7YY", __import__("sys").argv[1] + __import__("sys").argv[2] + __import__("sys").argv[3])]))
